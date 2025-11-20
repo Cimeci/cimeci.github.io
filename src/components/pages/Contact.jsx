8c7cb8897d8a9c9e13deb0ac5c3b33902db2d44b
Cimeci <Ilan.nowakglandier@outlook.fr>
2025-11-20 10:13:11 +0100

    Updates

--- src/components/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-
-export const Contact = () => {
-	const { t } = useTranslation()
-	return (
-		<div>{t('contact.title')}</div>
-	)
-}
